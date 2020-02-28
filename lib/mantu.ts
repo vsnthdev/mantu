e095041a05739e6afee38abdcf3f06d147643e7d
Vasanth Developer <vasanth@vasanthdeveloper.com>
2020-02-29 00:14:30 +0530

    💣 Big Bang!

---
+++ lib/mantu.ts
@@ -0,0 +1,8 @@
+//   ___    __________   |  Vasanth Developer (Vasanth Srivatsa)
+//   __ |  / /___  __ \  |  ------------------------------------------------
+//   __ | / / __  / / /  |  https://github.com/vasanthdeveloper/mantu.git
+//   __ |/ /  _  /_/ /   |
+//   _____/   /_____/    |  Entryfile for mantu project
+//                       |
+
+console.log('Hello World!')
