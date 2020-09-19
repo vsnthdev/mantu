7de0bb5017ea8e54fca4caa6441e56aaf13355a9
Vasanth Developer <vasanth@vasanthdeveloper.com>
2020-09-19 23:02:35 +0530

    ✨ added itivrutaha logger
    1. Added chalk, knex, itivrutaha and pg as dependencies

---
+++ src/logger/index.js
@@ -0,0 +1,16 @@
+/*
+ *  This file will log application log events.
+ *  Created On 18 September 2020
+ */
+
+import chalk from 'chalk'
+import itivrutaha from 'itivrutaha'
+
+export default itivrutaha.createNewLogger({
+    theme: `🤖 ${chalk.cyanBright('APP')} [${chalk.gray(
+        ':time',
+    )}] :type :message`,
+    timeFormat: 'hh:MM:ss TT, dS mmm yyyy',
+    boldType: true,
+    typeCase: 0,
+})
--- src/mantu.js
+++ src/mantu.js
@@ -1,1 +1,10 @@
-console.log('hey 👋')
+import logger from './logger/index.js'
+
+logger.info('hey 👋')
+
+// startup order:
+//     1. connect to the database
+//     2. login to Discord
+//     3. start the web server
+//     4. run the tasks initially
+//     5. run puppeteer automation if required
