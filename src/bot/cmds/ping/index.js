198a5765d39bd37630f6162b93f2e9df061fc704
Vasanth Developer <vasanth@vasanthdeveloper.com>
2020-11-06 19:45:24 +0530

    ✨ ping command now responds with Ping and Latency info

--- src/bot/cmds/ping/index.js
+++ src/bot/cmds/ping/index.js
@@ -3,9 +3,20 @@
  *  Created On 28 September 2020
  */
 
-const action = async (msg, args) => {
-    console.log('action for ping command')
-}
+import { MessageEmbed } from 'discord.js'
+import discord from '../../discord/index.js'
+
+const action = async msg =>
+    await discord.messages.sendEmbed(
+        new MessageEmbed()
+            .addField('Ping', `${Math.round(msg.client.ws.ping)}ms`, true)
+            .addField(
+                'Latency',
+                `${Date.now() - msg.createdTimestamp}ms`,
+                true,
+            ),
+        msg,
+    )
 
 export default {
     action,
