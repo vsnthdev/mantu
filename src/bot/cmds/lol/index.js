cf1d8123fb1dd2c6e6e465fd6439f36a1944d22c
Vasanth Developer <vasanth@vasanthdeveloper.com>
2021-01-11 20:54:53 +0530

    ✨ added lol command

---
+++ src/bot/cmds/lol/index.js
@@ -0,0 +1,74 @@
+/*
+ *  Responds with a random meme from Reddit related to programming.
+ *  Created On 11 January 2021
+ */
+
+import axios from 'axios'
+import { MessageEmbed } from 'discord.js'
+
+import { config } from '../../../config/index.js'
+import { discord } from '../../discord/index.js'
+
+// we'll pick one depending on the request
+const list = {
+    default: 'ProgrammerHumor',
+    alt: 'techhumor',
+    it: 'ITMemes',
+    'tech support': 'techsupportmemes',
+}
+
+const action = async msg => {
+    const req =
+        msg.content.slice(config.get('discord.prefix').length + 3).trim() ||
+        'default'
+
+    if (!list[req])
+        return await discord.messages.sendEmbed(
+            new MessageEmbed().setDescription(
+                `The requested subreddit "${req}" isn't allowed.`,
+            ),
+            msg,
+        )
+
+    // request a sub-reddit in JSON format from Reddit
+    let {
+        data: {
+            data: { children: res },
+        },
+    } = await axios({
+        method: 'GET',
+        url: `https://www.reddit.com/r/${list[req]}.json`,
+    })
+
+    // remove 18+, and keep only
+    // Reddit image ones
+    res = res.map(post => post.data)
+    res = res.filter(post => !post.is_video)
+    res = res.filter(post => !post.over_18)
+    res = res.filter(post => !post.media)
+    res = res.filter(post => post.url.includes('redd.it'))
+
+    // remove all keys that we don't need
+    res = res.map(post => {
+        return {
+            url: `https://reddit.com${post.permalink}`,
+            title: post.title,
+            thumbnail: post.url,
+        }
+    })
+
+    const meme = res[Math.floor(Math.random() * res.length)]
+
+    return await discord.messages.sendEmbed(
+        new MessageEmbed()
+            .setTitle(meme.title)
+            .setURL(meme.url)
+            .setImage(meme.thumbnail),
+        msg,
+    )
+}
+
+export default {
+    action,
+    description: 'A random programming meme from Reddit.',
+}
